a143c2d7015a3552ea355d62344400188771dc67
SinghYuvraj0506 <singhyuvraj0506@gmail.com>
2024-03-27 07:58:59 +0530

    cors, cookie parser and middlewares setup ready

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config()
-
-const app = express()
+dotenv.config();
 
 connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error occured in express server", err);
+      throw err;
+    });
 
-app.listen(process.env.PORT,()=>{
-    console.log("Server running at port",process.env.PORT)
-})
-
-
-
-
-
+    app.listen(process.env.PORT, () => {
+      console.log("Server running at port", process.env.PORT);
+    });
+  })
+  .catch(() => {
+    console.log("MongoDB connection Failed");
+  });
