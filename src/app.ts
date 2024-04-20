08a96babfa6c508652c57478f49e85960dfe1f43
Yuvraj <singhyuvraj0506@gmail.com>
2024-04-20 11:50:23 +0530

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,10 @@ import session from "express-session"
 const app = express()
 
 
-app.use(cors(({
+app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true
-})))
+}))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
